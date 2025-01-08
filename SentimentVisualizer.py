b501dd1f0e9fa4a9c353f332033c265b117982c7
Adrian Stangl <stangl.adrian@web.de>
2025-01-08 17:21:56 +0100

    remove pos, neg and net sentiment plots

--- SentimentVisualizer.py
+++ SentimentVisualizer.py
@@ -59,32 +59,9 @@ def plot_result(results, news_site, keyword, start_date="2018-01-01"):
     diff_dates = dates  # Dates for the second plot remain the same
     
     # Set up the figure with three subplots
-    fig, axs = plt.subplots(3, 1, figsize=(11, 12))
+    fig, axs = plt.subplots(1, 1, figsize=(8, 3))
+
     
-    # Plot Positive and Negative Sentiments
-    axs[0].plot(dates, positive_scores, label="Positive Sentiment", color="#0e7800", marker='o')
-    axs[0].plot(dates, negative_scores, label="Negative Sentiment", color="#ed1103", marker='x')
-    axs[0].set_title(f"Sentiment Analysis for {news_site} on {keyword} ({len(filtered_results)} articles)", fontsize=14)
-    axs[0].set_ylabel("Sentiment Score", fontsize=12)
-    axs[0].legend()
-    axs[0].grid(True)
-    axs[0].xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
-    axs[0].xaxis.set_major_locator(four_months)
-    axs[0].tick_params(axis="x", rotation=45)
-    axs[0].set_xlim(dates[0] - timedelta(days=3), dates[-1])  # Set x-axis limits
-    
-    # Plot Sentiment Difference
-    axs[1].plot(diff_dates, sentiment_diff, label="Sentiment Difference", color="orange", marker='s')
-    axs[1].plot(dates, diff_fn(x), linestyle="dashed", color="black")
-    axs[1].set_title("Sentiment Difference", fontsize=14)
-    axs[1].set_ylabel("Difference Score", fontsize=12)
-    axs[1].set_xlabel("Publishing Date", fontsize=12)
-    axs[1].legend()
-    axs[1].grid(True)
-    axs[1].xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
-    axs[1].xaxis.set_major_locator(four_months)
-    axs[1].tick_params(axis="x", rotation=45)
-    axs[1].set_xlim(dates[0] - timedelta(days=3), dates[-1])  # Set x-axis limits
 
     # Plot Monthly Article Counts (Histogram)
     # Normalize dates by stripping time precision
@@ -93,19 +70,16 @@ def plot_result(results, news_site, keyword, start_date="2018-01-01"):
     months = [date.replace(day=1) for date in dates]  # Normalize dates to the first day of the month
     unique_months, article_counts = np.unique(months, return_counts=True)
 
-    axs[2].bar(unique_months, article_counts, width=20, color="#007acc")
-    axs[2].set_title(f"Number of Articles Published Per Month for {news_site} on keyword {keyword}", fontsize=14)
-    axs[2].set_ylabel("Article Count", fontsize=12)
-    axs[2].set_xlabel("Month", fontsize=12)
-    axs[2].grid(True)
-    axs[2].xaxis.set_major_locator(four_months)
-    axs[2].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m')) # Format as "YYYY-MM"
-    axs[2].tick_params(axis="x", rotation=45)
-    axs[2].yaxis.set_major_locator(MaxNLocator(integer=True))  # Ensure y-axis labels are integers
-    axs[2].set_xlim(unique_months[0] - timedelta(days=15), unique_months[-1] + timedelta(days=15))
-
-    # Apply y limits to all plots
-    axs[1].set_yticks(np.arange(-0.20, 0.3 + 0.01, 0.05))
+    axs.bar(unique_months, article_counts, width=20, color="#007acc")
+    axs.set_title(f"Number of Articles Published Per Month for {news_site} on keyword {keyword}", fontsize=14)
+    axs.set_ylabel("Article Count", fontsize=12)
+    axs.set_xlabel("Month", fontsize=12)
+    axs.grid(True)
+    axs.xaxis.set_major_locator(four_months)
+    axs.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m')) # Format as "YYYY-MM"
+    axs.tick_params(axis="x", rotation=45)
+    axs.yaxis.set_major_locator(MaxNLocator(integer=True))  # Ensure y-axis labels are integers
+    axs.set_xlim(unique_months[0] - timedelta(days=15), unique_months[-1] + timedelta(days=15))
 
     # Add to global plots for comparisons
     global_axs[0].plot(dates, diff_fn(x), label=news_site)
